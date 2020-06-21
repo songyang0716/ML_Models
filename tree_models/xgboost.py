509f5f65647a216c7daf495ca88431fb05db3496
songyang0716 <yangsong0716@yahoo.com>
2020-06-20 19:12:53 -0700

    keep working on xgboost

--- tree_models/xgboost.py
+++ tree_models/xgboost.py
@@ -10,8 +10,9 @@ class XGB:
 				 learning_rate=0.1,
 				 reg_lambda=1,
 				 gamma=0,
-				 min_child_sample = None,
+				 min_child_sample = 10,
 				 min_child_weight=1,
+				 features=None,
 				 objective='linear'):
 	
 		self.base_score = base_score
@@ -23,6 +24,7 @@ class XGB:
 		self.min_child_sample = min_child_sample
 		self.min_child_weight = min_child_weight
 		self.objective = objective
+		self.features=features
 
 	def xgb_tree(self, X, w, m_depth):
 		"""
@@ -34,27 +36,45 @@ class XGB:
 			return 
 		bst_var, bst_cut = None, None 
 		max_gain = 0
+		n_sample = X.shape[0]
 		G_left_best, G_right_best, H_left_best, H_right_best = 0,0,0,0
-
 		# go through all the values of each feature, and record the maximum gain point
+		for feature in self.features:
+			for cut_value in np.unique(X[features].values):
+				if self.min_child_sample:
+					left_bin_size = np.sum(X[feature] <= cut_value)
+					if (left_bin_size < self.min_child_sample) or (n_sample - left_bin_size < self.min_child_sample):
+						continue
+	
+				G_left  = np.sum(X.loc[X[feature] <= cut_value,'g'])
+				G_right = np.sum(X.loc[X[feature] > cut_value,'g'])
+				H_left  = np.sum(X.loc[X[feature] <= cut_value,'h'])
+				H_right = np.sum(X.loc[X[feature] > cut_value,'h'])
+
+				#https://stats.stackexchange.com/questions/317073/explanation-of-min-child-weight-in-xgboost-algorithm
+				
+		return 	
 
 
 
 def main():
-	xg_boost_estimator = XGB()
+	xg_boost_estimator = XGB(features=['x1','x2'])
 	print(xg_boost_estimator.max_depth)
+	print(xg_boost_estimator.features)
 
 	# create some fake dataset to fit 
-	sample_size = 1000
+	sample_size = 100
 	x1 = np.random.uniform(-10, 10, sample_size)
 	x2 = np.random.normal(-5, 5, sample_size)
 	y = 3*x1**3 - 2*x2**2 + 10*x2 + np.random.normal(0, 1, sample_size)
 
 	df = pd.DataFrame({'x1':x1,
-		 		       'x2':x2,
-		 		       'y':y})
+					   'x2':x2,
+					   'y':y})
 	print (df.shape)
 	print (df.columns)
+	feature = 'x1'
+	print (np.unique(df[feature].values))
 
 
 
